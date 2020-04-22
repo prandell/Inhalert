3afcb1d1cb83b519c61b59783f4d845e6254aaaf
Patrick Randell <prandell@student.unimelb.edu.au>
2020-04-22 10:28:30 +0800

    Changed authentication requirements/routes

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 
 // Dashboard
-router.get('/', ensureAuthenticated, (req, res) =>
+router.get('/',  (req, res) =>
     res.render('dashboard', {
         user: req.user
     })
