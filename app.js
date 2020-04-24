ccaf3f15dccb9cab832676bbd3fd9ef148437765
Patrick Randell <prandell@student.unimelb.edu.au>
2020-04-24 18:15:51 +0800

    trying to get heroku working #2

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'));
 app.use(express.json());
-app.use(methodOverride('_method'))
 
 //Body parser (req.body)
 app.use(express.urlencoded({ extended: false }));
