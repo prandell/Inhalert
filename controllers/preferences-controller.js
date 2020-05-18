bd44d759b8e8ea600e25e81ff07205327a641fa1
Chang Yi Lee <changyi@student.unimelb.edu.au>
2020-05-18 13:10:00 +1000

    users able to subscribe more than one site at a time

--- controllers/preferences-controller.js
+++ controllers/preferences-controller.js
@@ -5,31 +5,55 @@ const Site = mongoose.model('Site');
 const SiteSub = mongoose.model('SiteSub')
 
 const selectSite = function(req, res) {
+
     const {siteSelection} = req.body
     const siteId = siteSelection
-    Site.findOne({siteId: siteId}).then(site => {
-        if(!site) {
-            errors = [{msg: 'Site could not be found'}]
-            res.render('preferences', {
-                errors: errors
-            });
-        } else {
-            const newSiteSub = new SiteSub({
-                email: req.user.email,
-                siteId: site.siteId
-            });
-            newSiteSub
-                .save()
-                .then(siteSub => {
-                    req.flash(
-                        'success_msg',
-                        'You will now receive alerts for the selected site'
-                    );
-                    res.redirect('/dashboard');
+    console.log('request body : ', siteId);
+    for (let i = 0; i < siteId.length; i++) {
+        Site.findOne({siteId: siteId[i]}).then(site => {
+            if (!site) {
+                errors = [{msg: 'Site could not be found'}]
+                res.render('preferences', {
+                    errors: errors
+                });
+            } else {
+                SiteSub.findOne({email: req.user.email, siteId: siteId[i]}, function (err, result) {
+                    if(err) {
+                        console.log(err);
+                    }
+                    if(!result) {
+                        const newSiteSub = new SiteSub({
+                            email: req.user.email,
+                            siteId: site.siteId
+                        });
+                        newSiteSub
+                            .save()
+                            .then(siteSub => {
+                                console.log(siteSub);
+                                req.flash(
+                                    'success_msg',
+                                    'You will now receive alerts for the selected site'
+                                );
+                                ///res.redirect('/dashboard');
+                            })
+                            .catch(err => console.log(err));
+                    }
                 })
-                .catch(err => console.log(err));
+            }
+        })
+    }
+}
+
+const userSubscribed = function (req, res) {
+    SiteSub.find({email: req.user.email}, function(err, result) {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log(result)
         }
-    });
+
+    })
 }
 
 module.exports.selectSite = selectSite;
+module.exports.userSubscribed = userSubscribed;
--- public/static/sites.js
+++ public/static/sites.js
@@ -1,3 +1,4 @@
+
 function populateSelect() {
     $.getJSON("/static/sites.json", function (data) {
         var select = document.getElementById('sel');
@@ -10,6 +11,49 @@ function populateSelect() {
 
 function show(element) {
     var msg = document.getElementById('msg');
-    msg.innerHTML = 'Selected: <b>' + element.options[element.selectedIndex].text + '</b> </br>' +
-        'ID: <b>' + element.value + '</b>';
+    var value = element.options[element.selectedIndex].text.replace( /\s/g, '');
+    var ele = document.getElementById(value);
+    if(!ele) {
+        msg.innerHTML +="<li class = 'subs' id ="+ element.options[element.selectedIndex].text.replace( /\s/g, '') + ' value =' + element.value + "> Selected: " + element.options[element.selectedIndex].text + "<span class='close'>x</span></li>";
+    }
+        ///+'ID: <b>' + element.value + '</b> </li> ';
+}
+
+function removeSelected() {
+    /* Get all elements with class="close" */
+    let closebutton = document.getElementsByClassName('close');
+    let i;
+    console.log(closebutton.length);
+    for (i = 0; i< closebutton.length; i++) {
+        closebutton[i].addEventListener("click", function () {
+            this.parentNode.parentNode.removeChild(this.parentNode);
+            ///this.parentElement.style.display = 'none';
+            ///this.parentElement.innerHTML = 'none';
+            console.log('button clicked');
+        });
+    }
+}
+
+async function getSubs() {
+    const subs = document.getElementsByClassName('subs');
+    let data = [];
+    for (let i = 0; i < subs.length; i++) {
+        data.push(subs[i].getAttribute('value'));
+    }
+    let siteToSub = {
+        siteSelection: data
+    }
+    console.log(siteToSub);
+    try {
+        const response = await fetch('/users/preferences', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(siteToSub)
+        })
+                    console.log('completed!',response);
+        } catch(err) {
+            console.log(err);
+        }
 }
