9fd1c306dc7500ab3f95be93e546f1bed953884e
Patrick Randell <prandell@student.unimelb.edu.au>
2020-06-02 18:02:00 +0800

    Allowed for searching via suburb

--- public/static/convert_postcode.js
+++ public/static/convert_postcode.js
@@ -1,61 +1,115 @@
 function convert_postcode() {
     let postcode = document.getElementById('whatsup').value
-
+    document.getElementById('enter').innerText = 'Loading ...'
     //Checking bad values
-    // if (postcode.length != 4 || isNaN(postcode)) {
-    //     window.location = '/dashboard/siteSummary';
-    // }
-    $.ajax({
-        url: "https://cors-anywhere.herokuapp.com/http://v0.postcodeapi.com.au/suburbs/" + postcode +
-            ".json",
-        beforeSend: function (xhrObj) {
-            // Request headers
-            xhrObj.setRequestHeader("Accept", "application/json");
-        },
-        type: "GET",
-        // Request body
-        data: "",
-    }).done(function (data) {
-        //Postcode doesnt exist
-        if (!data[0]) {
-            console.log("Failed at postcode")
-            // window.location = '/dashboard/siteSummary';
-            return
-        }
-
-        //Using longitude/latitude of postcode
-        var location = `[${data[0].latitude}, ${data[0].longitude}]`
-        var params = {
-            // Request parameters
-            "location": location
-        };
+    if (isNaN(postcode)) {
         $.ajax({
-            url: "https://cors-anywhere.herokuapp.com/https://gateway.api.epa.vic.gov.au/environmentMonitoring/v1/sites?environmentalSegment=air&" + $.param(params),
+            url: "https://cors-anywhere.herokuapp.com/http://v0.postcodeapi.com.au/suburbs.json?" + "name="+postcode+"&state=VIC",
             beforeSend: function (xhrObj) {
                 // Request headers
-                xhrObj.setRequestHeader("X-API-Key", "050c16c08ef84cadb8f92d5d73074b95");
+                xhrObj.setRequestHeader("Accept", "application/json");
             },
             type: "GET",
             // Request body
             data: "",
         }).done(function (data) {
-            //No site close to this postcode (ie, its not Victorian)
-            if (!data.records|| !data.records[0].siteID) {
-                console.log("failed at EPA")
+            //Postcode doesnt exist
+            if (!data[0]) {
+                console.log("Failed at postcode")
                 window.location = '/dashboard/siteSummary';
                 return
             }
 
-            //Display the summary
-            var siteId = data.records[0].siteID
-            // window.location.href = '/dashboard/siteSummary/'+siteId
+            //Using longitude/latitude of postcode
+            var location = `[${data[0].latitude}, ${data[0].longitude}]`
+            var params = {
+                // Request parameters
+                "location": location
+            };
+            $.ajax({
+                url: "https://cors-anywhere.herokuapp.com/https://gateway.api.epa.vic.gov.au/environmentMonitoring/v1/sites?environmentalSegment=air&" + $.param(params),
+                beforeSend: function (xhrObj) {
+                    // Request headers
+                    xhrObj.setRequestHeader("X-API-Key", "050c16c08ef84cadb8f92d5d73074b95");
+                },
+                type: "GET",
+                // Request body
+                data: "",
+            }).done(function (data) {
+                //No site close to this postcode (ie, its not Victorian)
+                if (!data.records|| !data.records[0].siteID) {
+                    console.log("failed at EPA")
+                    window.location = '/dashboard/siteSummary';
+                    return
+                }
+
+                //Display the summary
+                var siteId = data.records[0].siteID
+                window.location.href = '/dashboard/siteSummary/'+siteId
+            });
+        }).fail( function() {
+
+            //Any other failure
+            console.log("failed")
+            window.location = '/dashboard/siteSummary'
+            return
         });
-    }).fail( function() {
+    } else {
+        if (postcode.length!= 4) {
+            window.location = '/dashboard/siteSummary'
+            return
+        }
+        $.ajax({
+            url: "https://cors-anywhere.herokuapp.com/http://v0.postcodeapi.com.au/suburbs/" + postcode +
+                ".json",
+            beforeSend: function (xhrObj) {
+                // Request headers
+                xhrObj.setRequestHeader("Accept", "application/json");
+            },
+            type: "GET",
+            // Request body
+            data: "",
+        }).done(function (data) {
+            //Postcode doesnt exist
+            if (!data[0]) {
+                console.log("Failed at postcode")
+                window.location = '/dashboard/siteSummary';
+                return
+            }
 
-        //Any other failure
-        console.log("failed")
-        // window.location = '/dashboard/siteSummary'
-        return
-    });
+            //Using longitude/latitude of postcode
+            var location = `[${data[0].latitude}, ${data[0].longitude}]`
+            var params = {
+                // Request parameters
+                "location": location
+            };
+            $.ajax({
+                url: "https://cors-anywhere.herokuapp.com/https://gateway.api.epa.vic.gov.au/environmentMonitoring/v1/sites?environmentalSegment=air&" + $.param(params),
+                beforeSend: function (xhrObj) {
+                    // Request headers
+                    xhrObj.setRequestHeader("X-API-Key", "050c16c08ef84cadb8f92d5d73074b95");
+                },
+                type: "GET",
+                // Request body
+                data: "",
+            }).done(function (data) {
+                //No site close to this postcode (ie, its not Victorian)
+                if (!data.records|| !data.records[0].siteID) {
+                    console.log("failed at EPA")
+                    window.location = '/dashboard/siteSummary';
+                    return
+                }
 
+                //Display the summary
+                var siteId = data.records[0].siteID
+                window.location.href = '/dashboard/siteSummary/'+siteId
+            });
+        }).fail( function() {
+
+            //Any other failure
+            console.log("failed")
+            window.location = '/dashboard/siteSummary'
+            return
+        });
+    }
 }
